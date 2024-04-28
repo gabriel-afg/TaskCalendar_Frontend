2935fbff3473a4b098cbf21d8644f8a7f719d4f2
Gabriel Augusto <Gabriel.afg182@gmail.com>
2024-04-27 23:25:55 -0300

    Refactor: Refactor SideBar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 export function Sidebar({ children }: { children: React.ReactNode }) {
 	return (
-		<div className="p-7 w-full max-w-[300px] bg-[#DDE4F1]">{children}</div>
+		<div className="flex flex-col p-7 w-full min-h-[600px] max-w-[300px] bg-[#DDE4F1]">{children}</div>
 	);
 }
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -2,23 +2,27 @@ import { MainMenu } from '@/components/Menu/MainMenu';
 import { SecondaryMenu } from '@/components/Menu/SecondaryMenu';
 import { Sidebar } from '@/components/Sidebar/Sidebar';
 import InputTask from '@/components/Task/InputTask';
+import ListItem from '@/components/Task/ListItem';
 
 export default function Main() {
 	return (
 		<main className="p-7">
 			<div className="container mx-auto bg-white flex rounded-[20px] shadow-md overflow-hidden">
 				<Sidebar>
-					<div className="flex items-center mb-7">
-						<span className="w-9 h-9 bg-slate-800 rounded-md mr-4"></span>{' '}
-						MyTask
-					</div>
+					<div className="flex flex-col justify-between h-full">
+						<div>
+							<div className="flex items-center mb-7">
+								<span className="w-9 h-9 bg-slate-800 rounded-md mr-4"></span> MyTask
+							</div>
 
-					<div className="grid ">
-						<div className="grid gap-10">
-							<MainMenu />
-							<SecondaryMenu />
+							<div className="grid ">
+								<div className="grid gap-10">
+									<MainMenu />
+								</div>
+							</div>
 						</div>
-						<div className="flex items-center">
+
+						<div className="flex items-center mt-auto">
 							<span className="w-16 h-16 bg-neutral-50 border-4 border-emerald-400 rounded-full mr-4"></span>
 							<p className="text-[#56577E] font-bold text-cl">Gabriel Augusto</p>
 						</div>
@@ -29,6 +33,20 @@ export default function Main() {
 					<div>
 						<h2 className='mb-8'>Hoje</h2>
 						<InputTask />
+						<div className='flex flex-col w-full'>
+							<h3>Para fazer</h3>
+
+							<ul className='flex w-full'>
+								<li className='w-full'>
+									<ListItem
+										dateToDo='13 Set. 23'
+										isDone={true}
+									>
+										Estudar React
+									</ListItem>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
 			</div>
