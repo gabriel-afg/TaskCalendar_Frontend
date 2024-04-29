2c27d8596a79cddd4f3bf0121c1921d3bf0c5289
Gabriel Augusto <Gabriel.afg182@gmail.com>
2024-04-29 14:48:14 -0300

    feat: Search Bar implementation

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -4,7 +4,6 @@ import { Sidebar } from '@/components/Sidebar/Sidebar';
 import InputTask from '@/components/Task/InputTask';
 import ListItem from '@/components/Task/ListItem';
 import api from '@/service/axios';
-import { GetServerSideProps } from 'next';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
@@ -20,24 +19,36 @@ export default function Main() {
 	const [selectedPeriod, setSelectedPeriod] = useState('all');
 	const [tasks, setTasks] = useState([]);
 	const [taskCountToday, setTaskCountToday] = useState(0);
-  const [taskCountWeek, setTaskCountWeek] = useState(0);
-  const [taskCountMonth, setTaskCountMonth] = useState(0);
+	const [taskCountWeek, setTaskCountWeek] = useState(0);
+	const [taskCountMonth, setTaskCountMonth] = useState(0);
 	const [taskCountAll, setTaskCountAll] = useState(0);
+	const [searchValue, setSearchValue] = useState('');
+	const [searchResults, setSearchResults] = useState(null);
 
 	useEffect(() => {
-    const fetchTasks = async (period: string, setTaskCount: (count: number) => void) => {
-      const response = await api.get(`/tasks/${period}`);
-      setTaskCount(response.data.length);
-      if (period === selectedPeriod) {
-        setTasks(response.data);
-      }
-    };
+		if (searchValue) {
+			api.get(`/tasks/title/${searchValue}`)
+				.then(response => setSearchResults(response.data))
+				.catch(error => console.error(error));
+		} else {
+			setSearchResults(null);
+		}
+	}, [searchValue]);
 
-    fetchTasks('today', setTaskCountToday);
-    fetchTasks('week', setTaskCountWeek);
-    fetchTasks('month', setTaskCountMonth);
+	useEffect(() => {
+		const fetchTasks = async (period: string, setTaskCount: (count: number) => void) => {
+			const response = await api.get(`/tasks/${period}`);
+			setTaskCount(response.data.length);
+			if (period === selectedPeriod) {
+				setTasks(response.data);
+			}
+		};
+
+		fetchTasks('today', setTaskCountToday);
+		fetchTasks('week', setTaskCountWeek);
+		fetchTasks('month', setTaskCountMonth);
 		fetchTasks('all', setTaskCountAll);
-  }, [selectedPeriod]);
+	}, [selectedPeriod]);
 
 	return (
 		<main className="p-7">
@@ -53,12 +64,12 @@ export default function Main() {
 							<div className="grid ">
 								<div className="grid gap-10">
 									<MainMenu
-									selectedPeriod={selectedPeriod} 
-									taskCountToday={taskCountToday} 
-									taskCountWeek={taskCountWeek} 
-									taskCountMonth={taskCountMonth} 
-									setSelectedPeriod={setSelectedPeriod} 
-									taskCountAll={taskCountAll}
+										selectedPeriod={selectedPeriod}
+										taskCountToday={taskCountToday}
+										taskCountWeek={taskCountWeek}
+										taskCountMonth={taskCountMonth}
+										setSelectedPeriod={setSelectedPeriod}
+										taskCountAll={taskCountAll}
 									/>
 								</div>
 							</div>
@@ -77,8 +88,14 @@ export default function Main() {
 							<Icon icon="List" />
 							<h2 className='flex text-[#767798] items-center font-bold text-[16px] uppercase'>Hoje</h2>
 							<div>
-								<input className="border-2 ml-5 min-w-[300px] border-gray-300 bg-white h-10 px-3 rounded-lg text-sm focus:outline-none"
-									type="search" name="search" placeholder="Procurar Task por título" />
+								<input
+									className="border-2 ml-5 min-w-[300px] border-gray-300 bg-white h-10 px-3 rounded-lg text-sm focus:outline-none"
+									type="search"
+									name="search"
+									placeholder="Procurar Task por título"
+									value={searchValue}
+									onChange={e => setSearchValue(e.target.value)}
+								/>
 							</div>
 						</div>
 						<InputTask />
@@ -86,7 +103,7 @@ export default function Main() {
 							<h3 className='text-[#94A1B7] font-bold text-[12px] my-5'>Para fazer</h3>
 
 							<ul className='flex flex-col gap-2 w-full'>
-								{tasks.map((task: Task) => (
+								{(searchResults || tasks).map((task: Task) => (
 									<li className='w-full' key={task.id}>
 										<ListItem
 											dateToDo={new Date(task.date)}
